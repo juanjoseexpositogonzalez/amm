6a3069fbeabdceedeef0792d78efd15af34e827e
Juanjo <expositogonzalez.juanjose@gmail.com>
2025-10-30 06:49:06 +0100

    Adds Navbar

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    localhost: {},
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEYS}`,
+      accounts: privateKeys.split(",")
+    }
+  },
 };
--- src/components/App.js
+++ src/components/App.js
@@ -23,10 +23,19 @@ function App() {
     // Initiate provider
     const provider = loadProvider(dispatch)
 
+    // Fetch currents network chainId
     const chainId = await loadNetwork(provider, dispatch)
 
-    // Fetch accounts
-    await loadAccount(dispatch)
+    // Reload page when network changes
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload()
+    })
+
+    // Fetch current account from Metamask when changed
+    window.ethereum.on('accountsChanged', async () => {
+      await loadAccount(dispatch)
+    })
+
 
     // Initiate contracts
     await loadTokens(provider, chainId, dispatch)
@@ -39,7 +48,7 @@ function App() {
 
   return (
     <Container>
-      <Navigation account='0x0...' />
+      <Navigation />
 
       <h1 className='my-4 text-center'>React Hardhat Template</h1>
 
