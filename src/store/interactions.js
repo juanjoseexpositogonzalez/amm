68ffee9f915afd0fb03cf2cfdf0601ffd4087819
Juanjo <expositogonzalez.juanjose@gmail.com>
2025-10-31 06:37:27 +0100

    Added Swaps

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -14,7 +14,10 @@ import {
 
 import {
     setContract,
-    sharesLoaded
+    sharesLoaded,
+    swapRequest,
+    swapSuccess,
+    swapFail
 } from './reducers/amm'
 
 import TOKEN_ABI from '../abis/Token.json';
@@ -75,4 +78,34 @@ export const loadBalances = async (amm, tokens, account, dispatch) => {
 
     const shares = await amm.shares(account)
     dispatch(sharesLoaded(ethers.utils.formatUnits(shares.toString(), 'ether')))
+}
+
+// ------------------------------------------------------------------------------------
+// SWAP
+export const swap = async (provider, amm, token, symbol, amount, dispatch) => {
+    try {
+
+        dispatch(swapRequest())
+
+        let transaction
+
+        const signer = await provider.getSigner()
+
+        transaction = await token.connect(signer).approve(amm.address, amount)
+        await transaction.wait()
+
+        if (symbol === "DAPP") {
+            transaction = await amm.connect(signer).swapToken1(amount)
+        } else {
+            transaction = await amm.connect(signer).swapToken2(amount)
+        }
+        await transaction.wait()
+
+
+        dispatch(swapSuccess(transaction.hash))
+    } catch {
+
+        dispatch(swapFail())
+    }
+
 }
